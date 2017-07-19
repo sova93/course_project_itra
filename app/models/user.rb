dca6572511c877857a02300ce0fa1774fa78fb5d
sova93 <s_o_va@tut.by>
2017-07-19 18:46:56 +0300

    course project 2

---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable, :omniauth_providers => [:facebook, :vkontakte, :twitter, :google_oauth2]
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.oauth_data"] && session["devise.oauth_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
+  def is_email_used_different_provider
+    return User.where(email: self.email).where.not(provider: self.provider).size
+end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+      user.image = auth.info.image # assuming the user model has an image
+      user.skip_confirmation!
+    end
+  end
+  end
---
+++ config/routes.rb
@@ -0,0 +1,34 @@
+# Rails.application.routes.draw do
+#
+#   devise_for :users, :controllers => {
+#       omniauth_callbacks: 'omniauth_callbacks',
+#       sessions: 'custom_session'
+#   }
+#   # get 'home/index'
+#
+#   # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   root 'home#index'
+#   # get 'persons/profile', as: 'user_root'
+#
+# end
+
+Rails.application.routes.draw do
+
+  # We need to define devise_for just omniauth_callbacks:uth_callbacks otherwise it does not work with scoped locales
+  # see https://github.com/plataformatec/devise/issues/2813
+  devise_for :users, skip: [:session, :password, :registration, :confirmation], :controllers => {
+      omniauth_callbacks: 'omniauth_callbacks',
+  }
+  scope '(:locale)' do
+    # We define here a route inside the locale thats just saves the current locale in the session
+    get 'omniauth/:provider' => 'omniauth#localized', as: :localized_omniauth
+
+    devise_for :users, skip: [:omniauth_callbacks], controllers: {
+        # passwords: 'passwords',
+        # registrations: 'registrations',
+        sessions: 'custom_session'
+    }
+
+    root 'home#index'
+  end
+end
